9a5568e8abd759b5ac02de7f269f8aa538c690d8
Konrad Tomczuk <tomczuk.konrad@gmail.com>
2022-11-09 13:22:58 +0100

    Random color

--- RandomColor/ColorsDetailVC.swift
+++ RandomColor/ColorsDetailVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ColorsDetailVC.swift
 //  RandomColor
 //
 //  Created by Konrad Tomczuk on 07/11/2022.
@@ -7,13 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ColorsDetailVC: UIViewController {
 
+    var color: UIColor?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = color ?? .blue
+
         // Do any additional setup after loading the view.
     }
+    
 
 
 }
-
